9b04eba63f9c168f65199bdaadcc0eca55a73797
Jack Xia <jxia@pagerduty.com>
2022-01-14 14:28:30 -0500

    Add test

---
+++ hangman/test/impl_game_test.exs
@@ -0,0 +1,11 @@
+defmodule HangmanImplTest do
+  use ExUnit.Case
+  alias Hangman.Impl.Game
+
+  test "new game return correct state" do
+    game = Game.new_game
+    assert game.turns_left === 7
+    assert game.game_state === :initializing
+    assert length(game.letters) > 0
+  end
+end
