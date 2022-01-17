45fb753c4d7140ec46d13d4f4ff10bc80ff703c1
Jack Xia <jxia@pagerduty.com>
2022-01-17 11:01:05 -0500

    Complete text based client

--- text_client/lib/impl/player.ex
+++ text_client/lib/impl/player.ex
@@ -16,14 +16,40 @@ defmodule TextClient.Impl.Player do
 
 
   @spec interact(state) :: :ok
-  def interact({game, tally}) do
-    IO.puts feedback_for(tally)
-    # feedback
-    # display current word
-    # get next guess
-    # make a move
-    # interact()
+  def interact({_game, _tally = %{game_state: :won}}) do
+    IO.puts("Congratulations. You won!")
   end
+
+  def interact({ _game, tally = %{ game_state: :lost }}) do
+    IO.puts "Sorry, you've lost! The word was #{tally.letters |> Enum.join}"
+  end
+
+  @spec interact(state) :: :ok
+  def interact({game, tally}) do
+
+    IO.puts (feedback_for(tally))
+    IO.puts current_word(tally)
+    {updated_game, updated_tally } = Hangman.make_move(game, get_guess())
+    interact({updated_game, updated_tally})
+
+  end
+
+
+
+  def current_word(tally)  do
+    [
+    "Word so far ", tally.letters |> Enum.join(" "),
+    " turns left ", tally.turns_left |> to_string,
+    " used so far ", tally.used |> Enum.join(",")
+    ]
+  end
+
+  def get_guess() do
+    IO.gets("Next letter: ")
+    |> String.trim()
+    |> String.downcase()
+  end
+
 
   def feedback_for(tally = %{ game_state: :initializing}) do
     "Welcome! I'm thinking of a #{tally.letters |> length } letter word"
@@ -32,16 +58,9 @@ defmodule TextClient.Impl.Player do
   def feedback_for(%{game_state: :good_guess}), do: "Good guess"
   def feedback_for(%{game_state: :bad_guess}), do: "Letter not used"
   def feedback_for(%{game_state: :already_used}), do: "letter alread used in the letter"
+#   def feedback_for(%{game_state: :won}), do: "U won!"
 
 
-
-  def interact({ _game, _tally = %{ game_state: :won }}) do
-    IO.puts "Congrats, you've won!"
-  end
-
-  def interact({ _game, tally = %{ game_state: :lost }}) do
-    IO.puts "Sorry, you've lost! The word was #{tally.letters |> Enum.join}"
-  end
 
 
 end
