f30467ddad8e95885205e539e0bb636b8182f6f3
Jack Xia <jxia@pagerduty.com>
2022-01-18 11:03:30 -0500

    Add supervisor

--- dictionary/lib/impl/runtime/server.ex
+++ dictionary/lib/impl/runtime/server.ex
@@ -3,11 +3,19 @@ defmodule Dictionary.Runtime.Server do
   @type t :: pid()
   @me :wilma
   alias Dictionary.Impl.WordList
-  def start_link do
-    {:ok , pid} = Agent.start_link(&WordList.word_list/0, name: @me)
+
+  use Agent
+
+  def start_link(_arg) do
+    Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+
+    # if :rand.uniform < 0.33 do
+    #   Agent.get(@me, fn _ -> exit(:boom) end)
+    # end
+
     Agent.get(@me, &WordList.random_words/1)
   end
 
