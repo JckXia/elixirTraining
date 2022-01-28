517d05f3a587474655214e7ba04d3206816ee729
Jack Xia <jxia@pagerduty.com>
2022-01-28 11:34:15 -0500

    Add watch dog

--- hangman/lib/runtime/server.ex
+++ hangman/lib/runtime/server.ex
@@ -1,8 +1,10 @@
 defmodule Hangman.Runtime.Server do
 
   @type t :: pid
+  @idle_timeout 1 * 60 * 60 * 1000
   use GenServer
   alias Hangman.Impl.Game
+  alias Hangman.Runtime.Watchdog
   # Called in the context of client
   def start_link(_) do
     GenServer.start_link(__MODULE__,nil)
@@ -10,16 +12,18 @@ defmodule Hangman.Runtime.Server do
 
 
   def init(_args) do
-    {:ok, Game.new_game }
+    watcher = Watchdog.start(@idle_timeout)
+    {:ok, {Game.new_game, watcher}}
   end
 
-  def handle_call({:make_move, guess}, _from , game) do
+  def handle_call({:make_move, guess}, _from , {game, watcher }) do
+    Watchdog.im_alive(watcher)
     { updated_game, tally } =  Game.make_move(game, guess)
-    { :reply, tally, updated_game }
+    { :reply, tally, {updated_game, watcher} }
   end
 
-  def handle_call({:tally}, _from, game) do
-    {:reply, Game.tally(game), game }
+  def handle_call({:tally}, _from, {game, watcher}) do
+    {:reply, Game.tally(game), {game, watcher} }
   end
 
 end
