2c273c78137b12e2f07107811092067920678ce3
Jack Xia <jxia@pagerduty.com>
2022-01-28 10:30:36 -0500

    small bug fixes

--- b1/lib/b1_web/controllers/hangman_controller.ex
+++ b1/lib/b1_web/controllers/hangman_controller.ex
@@ -7,17 +7,29 @@ defmodule B1Web.HangmanController do
 
   def new(conn, __params) do
     game = Hangman.new_game()
-    tally = Hangman.tally(game)
     conn
     |> put_session( :game, game)
-    |> render("game.html", tally: tally)
+    |> redirect(to: Routes.hangman_path(conn, :show))
   end
 
   def update(conn, params) do
     guess = params["make_move"]["guess"]
-    game = get_session(conn, :game)
-    tally = Hangman.make_move(game, guess)
-    conn = put_in(conn.params["make_move"]["guess"], "")
+
+    put_in(conn.params["make_move"]["guess"],"")
+    |> get_session(:game)
+    |> Hangman.make_move(guess)
+
+
+    redirect(conn, to: Routes.hangman_path(conn, :show))
+
+  end
+
+  def show(conn, _param) do
+    tally =
+      conn
+      |> get_session(:game)
+      |> Hangman.tally()
     render(conn, "game.html", tally: tally)
   end
+
 end
